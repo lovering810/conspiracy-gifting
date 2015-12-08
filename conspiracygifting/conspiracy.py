de5a52afd7c278fa960daac154c90797077b04cd
Rebecca <rebecca@casecommons.org>
2015-12-07 20:43:56 -0500

    Moving things around and setting up json

--- conspiracygifting/conspiracy.py
+++ conspiracygifting/conspiracy.py
@@ -20,68 +20,74 @@ class ConspiracyCreator:
 	self.mailgun_url = mailgun_url
 	self.mailgun_from = mailgun_from
 	self.mailgun_api_key = mailgun_api_key
+	self.mailgun_endpoint = mailgun_endpoint
 	self.subject_template = email_subject
 	self.body_template = email_body
 	self.max_members = 100
 	self.group_size = 9
 
-  def _chunk_list(l, n):
+  def chunk_list(self, l, n):
 	for i in xrange(0, len(l), n):
 	  yield l[i:i+n]
-
-  def validate_members(self, members):
-	'''Takes members input and checks if
-	the input is valid.
-	The input must be a list of dicts.
-	Each member must be a dict with keys "name" and "email".
+  
+  def get_members(self, members):
+	'''Takes members input and processes.
 	The maximum number of members is defined in max_members.
 	'''
-	self.members = members
+	if len(members) > self.max_members:
+	  # Check that the number of members is valid
+	  return 'Needs less than ' + self.max_members + ' people in the list.'
+	else:
+	  self.members = list(self.chunk_list(members, 7))
 
+  def validate_members(self):
+	'''Takes member groups and checks if each is valid.
+	The group must be a list of dicts.
+	Each member must be a dict with keys "name" and "email".
+	'''
 	# start assuming the input is valid
 	valid_members = True
 
-	if not members or not isinstance(self.members, list):
+	if not self.members or not isinstance(self.members, list):
 	  # Check that there is input and the input is a list
 	  valid_members = False
 
-	for member in self.members:
-	  if not (isinstance(member, dict) and member.get('name') and member.get('email')):
-		# Check if each member is valid
-		print str(member) + ' is not valid'
+	for group in self.members:
+	  if not isinstance(group, list):
+		print str(group) + ' is not a list'
 		valid_members = False
-
-	if len(self.members) > self.max_members:
-	  # Check that the number of members is valid
-	  valid_members = False
+	  else:
+		for member in group:
+		  if not (isinstance(member, dict) and member.get('name') and member.get('email')):
+			# Check if each member is valid
+			print str(member) + ' is not valid'
+			valid_members = False
 
 	if not valid_members:
-	  print 'provide some json like [{"name": "joe", "email": "joe@example.com"}]!'
-	  return 'provide some json like [{"name": "joe", "email": "joe@example.com"}]!'
+	  print 'Something went wrong!'
+	  return 'Something went wrong!'
 	else:
 	  print 'Success!'
 
   def send_email(self):
 
-	member_groups = _chunk_list(self.members)
-	print list(member_groups)
-
 	# now email the members!
-	# for group in member_groups:
-	  # for member in group:
-		# pruned = [m for m in members if m != member]
-		# print pruned
-		# to = u','.join([m['email'] for m in pruned])
-		# subject = self.email_subject.format(**member)
-		# body = self.email_body.format(**member)
+	for group in member_groups:
+	  for member in group:
+		pruned = [m for m in members if m != member]
+		print pruned
+		to = u','.join([m['email'] for m in pruned])
+		subject = self.email_subject.format(**member)
+		body = self.email_body.format(**member)
 
-		# html = misaka.html(body)
-		# payload = {
-			# 'from': 'Conspiracy Gifting HQ <{}>'.format(self.mailgun_from),
-			# 'to': to,
-			# 'subject': subject,
-			# 'html': html
-		# }
+		html = misaka.html(body)
+		payload = {
+			'from': 'Conspiracy Gifting HQ <{}>'.format(self.mailgun_from),
+			'to': to,
+			'subject': subject,
+			'html': html
+		}
+		print payload
 		# response = requests.post(self.mailgun_endpoint, data=payload, auth=('api', self.mailgun_api_key))
 		# response.raise_for_status()
 
